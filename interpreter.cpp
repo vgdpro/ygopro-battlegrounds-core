8245a7b762f8926b2842ef812b381c0f9437e86c
purerosefallen <78877@qq.com>
2025-02-14 10:51:47 +0800

    remove

--- interpreter.cpp
+++ interpreter.cpp
@@ -113,8 +113,8 @@ interpreter::interpreter(duel* pd): coroutines(256) {
 	lua_setglobal(lua_state, "EFFECT_CHANGE_SUMMON_LOCATION_KOISHI");
 	lua_pushinteger(lua_state, EFFECT_LINK_SPELL_KOISHI);
 	lua_setglobal(lua_state, "EFFECT_LINK_SPELL_KOISHI");
-	lua_pushinteger(lua_state, EFFECT_SEA_PULSE);
-	lua_setglobal(lua_state, "EFFECT_SEA_PULSE");
+	// lua_pushinteger(lua_state, EFFECT_SEA_PULSE);
+	// lua_setglobal(lua_state, "EFFECT_SEA_PULSE");
 	lua_pushinteger(lua_state, EFFECT_MAP_OF_HEAVEN);
 	lua_setglobal(lua_state, "EFFECT_MAP_OF_HEAVEN");
 
