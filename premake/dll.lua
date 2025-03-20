78edd60d11a89270fc69ae6530b6697ce3592af5
purerosefallen <78877@qq.com>
2025-03-20 09:46:12 +0800

    support wasm build

--- premake/dll.lua
+++ premake/dll.lua
@@ -1,4 +1,5 @@
 newoption { trigger = "lua-dir", description = "", value = "PATH", default = "./lua" }
+newoption { trigger = "wasm", description = "" }
 
 function GetParam(param)
     return _OPTIONS[param] or os.getenv(string.upper(string.gsub(param,"-","_")))
@@ -9,13 +10,19 @@ if not os.isdir(LUA_DIR) then
     LUA_DIR="../lua"
 end
 
+WASM = GetParam("wasm")
+
 workspace "ocgcoredll"
     location "build"
     language "C++"
     cppdialect "C++14"
-
     configurations { "Release", "Debug" }
-    platforms { "x32", "x64" }
+    if WASM then
+        toolset "emcc"
+        platforms { "wasm" }
+    else 
+        platforms { "x32", "x64" }
+    end
     
     filter "platforms:x32"
         architecture "x32"
@@ -61,6 +68,10 @@ workspace "ocgcoredll"
         defines { "LUA_USE_LINUX" }
         buildoptions { "-fPIC" }
 
+    filter "system:emscripten"
+        defines { "LUA_USE_LONGJMP" }
+        buildoptions { "-fPIC" }
+
 filter {}
 
 include(LUA_DIR)
@@ -74,3 +85,7 @@ project "ocgcore"
     links { "lua" }
     
     includedirs { LUA_DIR .. "/src" }
+
+    filter "system:emscripten"
+        targetextension ".wasm"
+        linkoptions { "-s MODULARIZE=1", "-s EXPORT_NAME=\"createOcgcore\"", "--no-entry", "-s EXPORTED_FUNCTIONS=[\"_set_script_reader\",\"_set_card_reader\",\"_set_message_handler\",\"_create_duel\",\"_start_duel\",\"_end_duel\",\"_set_player_info\",\"_get_log_message\",\"_get_message\",\"_process\",\"_new_card\",\"_new_tag_card\",\"_query_card\",\"_query_field_count\",\"_query_field_card\",\"_query_field_info\",\"_set_responsei\",\"_set_responseb\",\"_preload_script\"]", "-s ENVIRONMENT=web,node", "-s EXPORTED_RUNTIME_METHODS=[\"ccall\",\"cwrap\",\"addFunction\",\"removeFunction\"]", "-s ALLOW_TABLE_GROWTH=1", "-s ALLOW_MEMORY_GROWTH=1", "-o ../wasm/ocgcore.js" }
